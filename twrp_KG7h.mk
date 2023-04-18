d88202fd3d6133c02329eef49605835454b23b5c
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-18 19:35:50 +0100

    Update and rename omni_TECNO-KG7h.mk to twrp_KG7h.mk

---
+++ twrp_KG7h.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from KG7h device
+$(call inherit-product, device/tecno/KG7h/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := KG7h
+PRODUCT_NAME := twrp_KG7h
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KG7h
+PRODUCT_MANUFACTURER := tecno
