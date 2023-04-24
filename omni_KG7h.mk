a304b3c3ca01cfaa13f065253d434954bd81c2f5
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-24 21:34:38 +0100

    Update omni_KG7h.mk

--- omni_KG7h.mk
+++ omni_KG7h.mk
@@ -8,8 +8,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
