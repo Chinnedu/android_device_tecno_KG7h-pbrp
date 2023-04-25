e00dc95dfe509e80ca6325d752567be08c103893
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-25 17:40:03 +0100

    Update omni_KG7h.mk

--- omni_KG7h.mk
+++ omni_KG7h.mk
@@ -6,29 +6,22 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from KG7h device
 $(call inherit-product, device/tecno/KG7h/device.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
-$(call inherit-product, vendor/pb/config/gsm.mk)
-
-# Extra required packages
-PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    libion.recovery
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := KG7h
-PRODUCT_NAME := omni_KG7h
-PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KG7h
-PRODUCT_MANUFACTURER := tecno
+PRODUCT_NAME := twrp_KG7h
+PRODUCT_BRAND := tecno
+PRODUCT_MODEL := KG7h
+PRODUCT_MANUFACTURER := Tecno Mobility Limited
+PRODUCT_RELEASE_NAME := Tecno Spark 8P
