47200f8f31082c41d86353347e99503c7252d16b
infinixs5lite <kelvinchinedu97@gmail.com>
2023-04-25 19:24:40 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
-    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
 
 PRODUCT_PACKAGES += \
     mtk_decrypt \
