b05c3dff171834b80371eb546bb788788bc532eb
infinixs5lite <kelvinchinedu97@gmail.com>
2023-05-07 06:59:49 +0100

    Update and rename omni_KG7h.mk to twrp_KG7h.mk

--- twrp_KG7h.mk
+++ twrp_KG7h.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/tecno/KG7h/device.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := KG7h
